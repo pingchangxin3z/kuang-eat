16973d67543ce7871f2b14aca661260590115dac
zzz <zuzhou.zhang@aminer.cn>
2026-03-19 15:46:56 +0800

    add Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <App />
   </StrictMode>
 )
